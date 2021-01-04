daef7a6d28dc0cfc52fd77e07955ffbf2fa6b6b9
YdO-ObY <ak7496@naver.com>
2021-01-05 00:53:07 +0900

    1000

---
+++ 1000 A+B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main()
+        {
+            string num = Console.ReadLine();
+            string[] s = num.Split();
+
+
+            int A = Int32.Parse(s[0]);
+            int B = Int32.Parse(s[1]);
+
+            Console.Write(A + B);
+
+
+
+        }
+    }
+}
